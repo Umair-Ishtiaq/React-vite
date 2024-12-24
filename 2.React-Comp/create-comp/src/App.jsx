ac665931ac13f56a57be2f1cddd941640bf08566
Umair-Ishtiaq <umairishtiaq04@gmail.com>
2024-12-24 07:44:33 -0800

    comp

---
+++ 2.React-Comp/create-comp/src/App.jsx
@@ -0,0 +1,13 @@
+import KgButtons from "./KgButtons";
+
+function App(){
+  return(
+    <>
+    <h1>
+      React+Vite Course 
+    </h1>
+    <KgButtons/>
+    </>
+  )
+}
+export default App;
---
+++ 2.React-Comp/create-comp/src/KgButtons.jsx
@@ -0,0 +1,6 @@
+function KgButtons() {
+    return (
+            <button onClick={() => props.onKgChange(3)}>3kg</button>
+    )
+}
+export default KgButtons;
