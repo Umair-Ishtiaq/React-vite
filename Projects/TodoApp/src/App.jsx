4e875fb95a74b56f32bd96da79d7981030444641
Umair Ishtiaq <umairishtiaq04@gmail.com>
2025-01-06 20:18:41 +0500

    proj 2

--- Projects/TodoApp/src/App.jsx
+++ Projects/TodoApp/src/App.jsx
@@ -2,16 +2,15 @@ import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 import TodoItem from "./components/TodoItem";
 import "./App.css";
+
 function App() {
   return (
-    <>
-      <center className="todo-container text-center">
-        <AppName />
-        <AddTodo />
-        <TodoItem />
-        {/* <TodoItem todoName="Buy groceries" todoDate="2022-02-01" /> */}
-      </center>
-    </>
+    <center className="todo-container text-center">
+      <AppName />
+      <AddTodo />
+      <TodoItem todoDate="2022-02-01" todoName="Buy groceries" />
+      <TodoItem todoDate="2024-02-01" todoName="Buy milk" />
+    </center>
   );
 }
 
--- Projects/TodoApp/src/components/TodoItem.jsx
+++ Projects/TodoApp/src/components/TodoItem.jsx
@@ -1,14 +1,10 @@
-function TodoItem() {
-  let todoName = "Learn React";
-  let todoDate = "01/01/2022";
+function TodoItem({ todoName, todoDate }) {
   return (
     <>
       <div className="container text-center">
         <div className="row mt-2">
           <div className="col-6">{todoName}</div>
-
           <div className="col-4">{todoDate}</div>
-
           <div className="col-2">
             <button className="btn btn-danger">Delete</button>
           </div>
